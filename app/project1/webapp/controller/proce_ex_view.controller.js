d43f372bb3d0f8a69eefc0773aa266907aab6971
Pavan Kumar <kumar.pavan@gmx.com>
2024-07-24 04:02:15 +0000

    ty

--- app/project1/webapp/controller/proce_ex_view.controller.js
+++ app/project1/webapp/controller/proce_ex_view.controller.js
@@ -21,6 +21,20 @@ function (Controller) {
                 console.log(data); // depending on your response targert your desired property.
             }
             
+        },
+
+        onPressBtn2: function () {
+            
+            var request = new XMLHttpRequest(); // Create a request variable and assign a new XMLHttpRequest object to it.
+            request.open('POST', '/odata/v4/catalog/deleteBook'); // Open a new connection, using the GET request on the URL endpoint
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send();
+
+            request.onload = async function () {
+                var data = JSON.parse(this.response);
+                console.log(data); // depending on your response targert your desired property.
+            }
+            
         }
 
 
